094569253431363de42f13c83052b2d6bd309c16
Anton Nebera <neberaanton@gmail.com>
2017-11-26 23:08:44 +0200

    New added vue project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Lato:100,100i,400,400i,700');
+#app {
+  font-family: 'Lato', sans-serif;
+}
+</style>
